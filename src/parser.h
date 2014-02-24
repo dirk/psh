f556c0d421962f164b4c06ec5ae1e67cf9e095ab
Dirk Gadsden <dirk@esherido.com>
2014-02-24 10:47:04 -0500

    Start adding syntax tree stuff

--- src/parser.h
+++ src/parser.h
@@ -11,6 +11,12 @@ typedef enum {
   TOKENS
 } token_type;
 
+typedef enum {
+  TSEQUENCE,
+  TCOMMAND,
+  TREES
+} tree_type;
+
 /*static const char *token_names[TOKENS] = {
   "keyword"
 };*/
@@ -32,15 +38,32 @@ typedef struct token_list {
   token** tokens;
 } token_list;
 
-token_list *scan_line(char *line);
-bool parse_token(char**, token**);
 
+typedef struct tree_command {
+  tree_type      type;
+  token_list*    list;
+} tree_command;
+typedef struct tree_sequence {
+  tree_type      type;
+  tree_command** commands;
+} tree_sequence;
+
+typedef struct tree {
+  tree_sequence** sequences;
+} tree;
+
+token_list *scan_line(char *line);
+tree *parse_list(token_list *list);
+
+
+// Scanning
+bool parse_token(char**, token**);
 token_word *consume_word(char**);
 token_word *consume_string(char**);
 token_keyword *consume_keyword(char**);
 
+// Utilities
 char consume_string_escape_sequence(char**);
-
 void print_token_list(token_list* tl);
 
 #endif
