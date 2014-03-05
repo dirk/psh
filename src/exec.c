51684f6f69b2a1fe03ad84f255db26b001d1cd4a
Dirk Gadsden <dirk@esherido.com>
2014-03-05 14:58:33 -0500

    Add path parsing

--- src/exec.c
+++ src/exec.c
@@ -2,9 +2,44 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <strings.h>
 
 #include "psh.h"
 #include "exec.h"
+
+static char *path[PATH_SIZE];
+
+void parse_path() {
+  char *op = getenv("PATH");
+  
+  int plen = strlen(op);
+  int ps = 0, pi = 0;// Start of segment and current index
+  int di = 0;// Destination (path) index
+  while(pi < plen) {
+    // If it's separator or end of string
+    if((op[pi] == ':' || op[pi] == '\0') && pi > ps) {
+      int segment_len = pi - ps;
+      char *s = malloc(sizeof(char) * (segment_len + 1));
+      s[segment_len] = '\0';
+      strncpy(s, &op[ps], segment_len);
+      // printf("segment: %s\n", s);
+      ps = pi + 1;
+      // Put it in the path
+      path[di] = s;
+      di += 1;
+    }
+    pi += 1;
+  }
+}
+char *find_in_path(char *f, char *dest) {
+  // TODO: Make it actually look through the path
+  return NULL;
+}
+
+void setup_exec() {
+  parse_path();
+}
+
 
 int exec_command(char *filename, char** argv) {
   int pid;
--- src/exec.h
+++ src/exec.h
@@ -1,6 +1,11 @@
 #ifndef EXEC_H
 #define EXEC_H
 
+// Maximum number of strings in the path
+#define PATH_SIZE 128
+
 extern char **environ;
+void setup_exec();
+char *find_in_path(char *file, char *dest);
 
 #endif
