bb1341aeeeac00e7a34791cc3b784041705d8d3f
Dirk Gadsden <dirk@esherido.com>
2014-03-06 16:50:19 -0500

    Add more tests

--- test/unit/test.c
+++ test/unit/test.c
@@ -8,9 +8,27 @@ TEST reader_test() {
   char *line = "ls .";
   token_list *list = scan_line(line);
   tree*       tr   = parse_list(list);
-  
+  // Check the first in sequence is a command
   tree_command* tc = tr->sequence[0];
   ASSERT_EQ(tc->type, TCOMMAND);
+
+  // Check length
+  int command_len = command_length(tc);
+  ASSERT_EQ(command_len, 2);
+
+  // Check the tokens are all correct
+  token_word *w;
+  // First token:
+  w = (token_word*)(tc->tokens[0]);
+  ASSERT_STR_EQ(w->contents, "ls");
+  // Second token:
+  w = (token_word*)(tc->tokens[1]);
+  ASSERT_STR_EQ(w->contents, ".");
+  // Ensure last is NULL:
+  ASSERT_EQ(tc->tokens[2], NULL);
+
+  // Check the second (last) in sequence is null.
+  ASSERT_EQ(tr->sequence[1], NULL);
   PASS();
 }
 
